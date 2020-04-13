f21b39d96c1a1cbf026666506243b96c554b7da4
Ma-G <magda0gie@gmail.com>
2020-04-13 21:59:28 +0200

    modify and add settings for mysql db

---
+++ todoapp/src/todoapp/settings/test.py
@@ -0,0 +1,15 @@
+from base import *
+import os
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('MYSQL_DATABASE', 'todoapp'),
+        'USER': os.environ.get('MYSQL_USER', 'todo'),
+        'PASSWORD': os.environ.get('MYSQL_PASSWORD', 'password'),
+        'HOST': os.environ.get('MYSQL_HOST', 'localhost'),
+        'PORT': os.environ.get('MYSQL_PORT', '3306'),
+
+    }
+}
